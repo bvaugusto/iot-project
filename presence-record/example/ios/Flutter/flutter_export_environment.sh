5bda4b73ae907d5ef2942101fd6ba883f0dc935f
Bruno <bvaugusto@gmail.com>
2020-03-29 16:46:12 -0300

    update enviroment

--- presence-record/example/ios/Flutter/flutter_export_environment.sh
+++ presence-record/example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/bvaugusto/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/bvaugusto/Iot/iot-project/class-checkin/example"
+export "FLUTTER_APPLICATION_PATH=/Users/bvaugusto/Iot/iot-project/presence-record/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
